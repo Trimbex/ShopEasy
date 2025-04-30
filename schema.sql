ad15cd976f6fcbb0e88b18a3e8f846d5dde2c425
Trimbex <111075379+Trimbex@users.noreply.github.com>
2025-04-30 22:22:46 +0300

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -4,7 +4,14 @@ CREATE TABLE users (
     name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
     profile_picture VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE refresh_tokens (
+    token TEXT PRIMARY KEY,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 -- PRODUCTS
